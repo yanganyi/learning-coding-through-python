6f8376febd9f3182ec04a6ec7ccfcbb0a9135944
yanganyi <ron.yanganyi@gmail.com>
2020-03-18 10:25:21 +0800

    new math test

---
+++ math-test/math-test-updated.py
@@ -0,0 +1,10 @@
+import random
+
+for i in range(10):
+    for j in range(3):
+        a = random.randint(1,10)
+        b = random.randint(1,10)
+        print(a, '*', b, '=', end='   ')
+    print()
+
+
